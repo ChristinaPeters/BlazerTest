c3c4c7c7e690d477f95a3c785721acde912f1524
Christina Peters <c.peters@archive-it.nl>
2024-11-15 10:35:08 +0100

    clean up

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazerTest;
-using Microsoft.Extensions.Options;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,12 +13,10 @@ var version = DataverseWebApiConfig.GetSection("Version").Value;
 var timeoutSeconds = int.Parse(DataverseWebApiConfig.GetSection("TimeoutSeconds").Value);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri("https://localhost:7276/authentication/login-callback"));
 
 // Create an named definition of an HttpClient that can be created in a component page
 builder.Services.AddHttpClient("DataverseClient", client =>
 {
-    // See https://learn.microsoft.com/powerapps/developer/data-platform/webapi/compose-http-requests-handle-errors
     client.BaseAddress = new Uri($"{resourceUrl}/api/data/{version}/");
     client.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
     client.DefaultRequestHeaders.Add("OData-Version", "4.0");
@@ -31,7 +28,6 @@ builder.Services.AddMsalAuthentication(options =>
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes
         .Add($"{resourceUrl}/user_impersonation");
-    //options.ProviderOptions.DefaultAccessTokenScopes.Add("api://your-api-client-id/.default"); // Scope for API access
 
 });
 
