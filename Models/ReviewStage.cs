ef17e3abb4006f1d1712d2f81529ea4a80dda2ef
Christina Peters <c.peters@archive-it.nl>
2024-12-02 12:08:25 +0100

    added changes to format the json, only it does not work yet!

---
+++ Models/ReviewStage.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazerTest.Models
+{
+    public class ReviewStage
+    {
+        public string Name { get; set; }
+        public List<string> ReviewersEmailAddresses { get; set; }
+        public int StageNumber { get; set; }
+    }
+
+    public class ReviewSettings
+    {
+        public List<ReviewStage> Stages { get; set; }
+    }
+
+}
